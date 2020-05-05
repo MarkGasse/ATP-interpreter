d463781a8500b20a1efec8f4807082e3bb4efe69
MarkGasse <mark.gasse@student.hu.nl>
2020-05-05 12:27:02 +0200

    Added parser (version 1)

--- Interperter.py
+++ Interperter.py
@@ -21,7 +21,10 @@ class Token():
         return self.type
     
     def __str__(self):
-        return ("| TOKEN: " + self.type + " | VALUE: " + self.value + " |")
+        if self.value != '\n':
+            return ("| TOKEN | type: " + self.type + " | value: " + self.value + " |")
+        else: 
+            return ("| TOKEN | type: " + self.type + " |")
 
 # -------------------------------------------
 # Token types
@@ -31,10 +34,12 @@ tokens = {
     'SUB': r'[-]',
     'MUL': r'[*]',
     'DIV': r'[/]',
-    'IS': r'[=]',
+    'ASSIGN': r'[=]',
     'EQUAL': r'[=][=]',
+    'EOL': r'[;]',
     'VAR': r'[a-zA-Z_][a-zA-Z0-9_]*',
-    'INT': r'[0-9_]'
+    'INT': r'[0-9_]',
+    
 }
 
 # -------------------------------------------
@@ -60,7 +65,7 @@ def matchToken(string : str, tokens : str) -> Union[Token,None]:
 
 #
 # lexer :: str, str -> [Union[Token,None]]
-def lexer(line : str, pattern : str) -> List[Union[Token,None]]:
+def lexer(line : str, pattern : str = '') -> List[Union[Token,None]]:
     
     if len(line) == 0:
         return []
@@ -82,6 +87,95 @@ def lexer(line : str, pattern : str) -> List[Union[Token,None]]:
 # Parser
 # -------------------------------------------
 
+class Number():
+    def __init__(self, token):
+        self.token = token
+
+    def __repr__(self):
+        return self.token
+
+    def __str__(self):
+        return ("Number(" + self.token.value + ")")
+
+class Operator(Number): 
+    pass
+
+    def __str__(self):
+        return ("Operator(" + self.token.value + ")")
+
+class Node():
+    def __init__(self, parent, Lchild, Rchild):
+        self.parent = parent
+        self.Lchild = Lchild
+        self.Rchild = Rchild
+
+    def __repr__(self):
+        return self.__str__()
+    
+    def __str__(self):
+        return (self.parent.token.type + "Operator("+ self.Lchild.__str__() + "," + self.parent.__str__() + "," + self.Rchild.__str__() + ")")
+
+# Create AST of current line
+# parseLine :: List[Token] -> Node
+def parseLine( tokens : List[Token]) -> Node:
+    if len(tokens) > 3: 
+        Lchild, parent, *tail = tokens
+        if re.match(r'[ASSIGN]', parent.type) != None: 
+            return Node(Operator(parent), Number(Lchild), parseOperators(tail))
+        else:
+            return []
+    elif len(tokens) == 3: 
+        Lchild, parent, Rchild = tokens
+        return Node(Operator(parent), Number(Lchild), Number(Rchild))
+    else: 
+        return []
+
+# Create AST of operators on current line
+# parseOperators :: List[Token] -> Node
+def parseOperators(tokens : List[Token]) -> Node: 
+    if len(tokens) < 3:
+        return tokens[0]
+
+    Lchild, parent, *tail = tokens
+    if re.match(r'[/^(MUL|DIV)$/]', parent.type) != None: 
+        Rchild, *tail = tail
+        tail.insert( 0,Node(Operator(parent), Lchild, Number(Rchild)))
+        return parseOperators(tail)
+    elif re.match(r'[/^(ADD|SUB)$/]', parent.type) != None:
+        Rchild, *tail = tail
+        if len(tail) >= 2:
+            return parseOperators(lookAhead(tokens, tail))
+        else: 
+            tail.insert(0, Node(Operator(parent), Lchild, Rchild))
+            return parseOperators(tail)
+
+# Check if next operator has higher priority (if it should be executed first)
+# lookAhead List[Token], List[Token] -> Node
+def lookAhead(tokens : List[Token], currentTail : List[Token]) -> Node: 
+    Lchild, parent, Rchild, parent2, Rchild2, *tail = tokens
+    if re.match(r'[/^(MUL|DIV)$/]', parent2.type) != None: 
+        tail.insert(0, Node(Operator(parent), Lchild, Node(Operator(parent2), Number(Rchild), Number(Rchild2))))
+        return tail
+    else: 
+        currentTail.insert(0, Node(Operator(parent), Number(Lchild), Number(Rchild)))
+        return currentTail
+
+# Create a List of Node from each line in txt file
+# parser :: List[Token], List[Token] -> List[Node]
+def parser(tokens : List[Token], Queue: List[Token] = []) -> List[Node]: 
+    if len(tokens) == 0: 
+        return []
+
+    head, *tail = tokens
+    if head.type != "EOL": 
+        Queue.append(head)
+        return parser(tail, Queue)
+    else:
+        return [parseLine(Queue)] + parser(tail, [])
+
+
+
+
 
 
 # -------------------------------------------
@@ -93,17 +187,12 @@ def lexer(line : str, pattern : str) -> List[Union[Token,None]]:
 # -------------------------------------------
 
 # Print list with tokens
-print(lexer(fileToStrings('File.txt'), ''))
+print(lexer(fileToStrings('File.txt')))
 
 #print str of each token
-for i in lexer(fileToStrings('File.txt'), ''): 
+for i in lexer(fileToStrings('File.txt')): 
     print(i)
 
-
-
-
-
-
-
-
+for x in parser(lexer(fileToStrings('File.txt'))):
+    print(x)
 
